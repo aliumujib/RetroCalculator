154937794e9af353b3231294a33154086bfd6e5a
aliumujib <aliuabdulmujib@gmail.com>
2017-05-13 21:44:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Calculator App
+Calculator app for nerds .. one day I just might put the on the store.. but till then sha.
+
+## Awesome notes, a note taking taking app built for travellers  
+
+<img src="Screens/CALC_SCREEN" width="250" />
+<img src="Screens/LAUNCH" width="250" />
+
+
+This app I built using assets from the iOS 10 course that I currently am taking, nothing fancy here! Completely stuck to the script…. Clone and enjoy, Ciao
+
+## Technology Stack
+
+** Stack views
+** Auto layout
+
+## TODO
+
+** Probably implement some scientific functions and what not but meh ¯\_(ツ)_/¯
+
+License
+----------------
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Aliu Abdul-Mujib
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,12 +7,136 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumberVal: String!
+    var leftHandVal: String!
+    var rightHandVal: String!
+    var resultVal: String!
+
+    var currentOperation = Operation.Empty
+    
+    //OUTLETS THEM
+    @IBOutlet weak var calcLabel: UILabel!
+    
+    //ACTION FIR THEM ALL NUMS
+    @IBAction func numberBtnPressed(_ sender: UIButton) {
+        playSound()
+        
+        runningNumberVal = runningNumberVal + "\(sender.tag)"
+        calcLabel.text = runningNumberVal
+    }
+    
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Mutiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let uri = URL(fileURLWithPath: path!)
+        
+        calcLabel.text = ""
+        runningNumberVal = ""
+        leftHandVal = ""
+        rightHandVal = ""
+        resultVal = ""
+
+        
+        do{
+            try btnSound = AVAudioPlayer(contentsOf: uri)
+            btnSound.prepareToPlay()
+            
+        }catch let error as NSError{
+            print(error.description)
+        }
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func processOperation(operation: Operation) {
+        if(currentOperation != Operation.Empty){
+            
+            if(runningNumberVal != ""){
+                rightHandVal = runningNumberVal
+                runningNumberVal = ""
+                
+                if(operation == Operation.Mutiply){
+                    resultVal  = "\(Double(leftHandVal)! * Double(rightHandVal)!)"
+                }else if(operation == Operation.Divide){
+                    resultVal  = "\(Double(leftHandVal)! / Double(rightHandVal)!)"
+                }else if(operation == Operation.Add){
+                    resultVal  = "\(Double(leftHandVal)! + Double(rightHandVal)!)"
+                }else if(operation == Operation.Subtract){
+                    resultVal  = "\(Double(leftHandVal)! - Double(rightHandVal)!)"
+                }
+                
+                leftHandVal = resultVal
+                calcLabel.text = resultVal
+                
+            }
+            currentOperation = operation
+        }else{
+        
+            leftHandVal = runningNumberVal
+            runningNumberVal = ""
+            currentOperation = operation
+        }
+    }
+    
+    
+    @IBAction func mutliplyBtnPressed(_ sender: UIButton) {
+        processOperation(operation: .Mutiply)
+    }
+    
+    @IBAction func divideBtnPressed(_ sender: UIButton) {
+        processOperation(operation: .Divide)
+
+    }
+
+    @IBAction func addBtnPressed(_ sender: UIButton) {
+        processOperation(operation: .Add)
+
+    }
+
+    @IBAction func subtractBtnPressed(_ sender: UIButton) {
+        processOperation(operation: .Subtract)
+
+    }
+
+    @IBAction func equalBtnPressed(_ sender: UIButton) {
+        processOperation(operation: currentOperation)
+    }
+    
+    @IBAction func clearBtnPressed(_ sender: UIButton) {
+        leftHandVal = "1"
+        rightHandVal = "1"
+        runningNumberVal = ""
+        resultVal = ""
+        calcLabel.text = ""
+        currentOperation = .Empty
+    }
+    
+    
+    func playSound() {
+        if(btnSound.isPlaying){
+            btnSound.stop()
+        }
+        
+        btnSound.play()
     }
 
     override func didReceiveMemoryWarning() {
